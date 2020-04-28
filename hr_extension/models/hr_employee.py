593d3a3238b43f0c16ad224db72752c282ac715a
M_Usama_I <usama@erpfy.co>
2020-04-28 12:09:39 +0500

    Add: Module for fields on Employee Contract for Payroll

---
+++ hr_extension/models/hr_employee.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from datetime import timedelta,datetime,date
+from odoo.exceptions import Warning, ValidationError
+
+
+# class HrEmployeeExtension(models.Model):
+# 	_inherit = 'hr.employee'
+
+
+
+
+	# employee_code = fields.Char("Employee Code")
+	# cost_card = fields.Many2one("sale.order", "Cost Card")
+	# wage = fields.Float("Wage")
+
